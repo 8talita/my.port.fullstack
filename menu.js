a467ad320e85e60235aa426783423bcc2593ac2a
Talita Priscila Farias Santos <119720253+8talita@users.noreply.github.com>
2023-12-16 20:40:31 -0300

    Add files via upload

---
+++ menu.js
@@ -0,0 +1,47 @@
+/*let var1 = 1;
+var var2 = "página não encontrada";
+const var3 = 234234;
+let var4 = var1 + var3;
+
+let boolean = true;
+let array = [2, "a", 3, true, "talita"];
+let pessoa = { nome: "talita", peso: 60, altura: 1.70 };
+
+alert(pessoa.peso);
+alert(pessoa.nome);
+
+let x = 1;
+let y = 10;
+
+let b = x + y;
+let a = b;
+let c = 12;
+
+let multiplicacao = x * y;
+let multiplicacao1 = 2 * 5;
+let multiplicacao2 = 2 * x;
+
+let divisao = 2 / 4;
+let divisao1 = x / y;
+let divisao2 = x / 2;
+
+let expressao = (2*x) + y;
+
+if(x == y){
+    console.log("x é igual a y");
+}else if(1 == 1){
+    console.log("1 é iguaal a 1");
+}
+else {
+    console.log("x é diferente de y");
+}
+alert("escreva a sua idade");
+let idade = prompt();
+
+if(idade >= 18 && idade <= 60){
+    console.log("adulto");
+}else{
+    console.log("menor de idade");
+}
+*/
+
